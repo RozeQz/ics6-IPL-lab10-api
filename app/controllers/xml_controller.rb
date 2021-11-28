fbe028a7c9c4da0c6e66859f11c698d4b4adbf06
RozeQz <liza.roz@yandex.ru>
2021-11-28 23:26:36 +0300

    First release

---
+++ app/controllers/xml_controller.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# XML controller class
+class XmlController < ApplicationController
+    def input; end
+
+    def view
+        @input = validate_input(params[:n])
+        @numbers = (0..@input).select { |i| palindrome?(i * i) }
+        @result = @numbers.size
+    rescue StandardError => e
+        @error = case e.class.to_s
+                 when 'ArgumentError' then 'Incorrect input. Do not use letters!'
+                 else e
+                 end
+    end
+    
+    private
+
+    def palindrome?(number)
+        number.to_s == number.to_s.reverse
+    end
+
+    def validate_input(input)
+        raise 'You have to enter something!' if input.empty?
+        Integer(input)
+    end
+end
+
+
