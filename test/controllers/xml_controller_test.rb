50491b277eefff877435425cb9c3dd1a9faff552
RozeQz <liza.roz@yandex.ru>
2021-11-29 02:52:39 +0300

    Release version. No tests

--- test/controllers/xml_controller_test.rb
+++ test/controllers/xml_controller_test.rb
@@ -1,10 +1,5 @@
 require "test_helper"
 
 class XmlControllerTest < ActionDispatch::IntegrationTest
-  test 'check rss format' do
-    get '/', params: { n: 10, format: :rss }
-    assert_response :success
-    assert_includes @response.headers['Content-Type'], 'application/rss'
-    # assert_includes @response.body
-  end
+  #
 end
