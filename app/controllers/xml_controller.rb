aec0b575097471f178f9927fdb5645ea89f5cb65
RozeQz <liza.roz@yandex.ru>
2021-11-29 14:05:26 +0300

    Run rubocop

--- app/controllers/xml_controller.rb
+++ app/controllers/xml_controller.rb
@@ -2,29 +2,28 @@
 
 # XML controller class
 class XmlController < ApplicationController
-    def input; end
+  def input; end
 
-    def view
-        @input = validate_input(params[:n])
-        @numbers = (0..@input).select { |i| palindrome?(i * i) }
-        @result = @numbers.size
-    rescue StandardError => e
-        @error = case e.class.to_s
-                 when 'ArgumentError' then 'Некорректный ввод. Используйте только цифры!'
-                 else e
-                 end
-    end
-    
-    private
+  def view
+    @input = validate_input(params[:n])
+    @numbers = (0..@input).select { |i| palindrome?(i * i) }
+    @result = @numbers.size
+  rescue StandardError => e
+    @error = case e.class.to_s
+             when 'ArgumentError' then 'Некорректный ввод. Используйте только цифры!'
+             else e
+             end
+  end
 
-    def palindrome?(number)
-        number.to_s == number.to_s.reverse
-    end
+  private
 
-    def validate_input(input)
-        raise 'Пустой ввод. Вы ничего не ввели!' if input.empty?
-        Integer(input)
-    end
+  def palindrome?(number)
+    number.to_s == number.to_s.reverse
+  end
+
+  def validate_input(input)
+    raise 'Пустой ввод. Вы ничего не ввели!' if input.empty?
+
+    Integer(input)
+  end
 end
-
-
