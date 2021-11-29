14c43e99383cbdc97d98165ad1a0b3696a8b55fc
RozeQz <liza.roz@yandex.ru>
2021-11-30 02:45:47 +0300

    Add test to check rss format

--- test/controllers/xml_controller_test.rb
+++ test/controllers/xml_controller_test.rb
@@ -3,4 +3,9 @@
 require 'test_helper'
 
 class XmlControllerTest < ActionDispatch::IntegrationTest
+  test 'should check rss format' do
+    get "#{xml_view_url}.rss", params: { input_number: 30 }
+    assert_response :success
+    assert_includes @response.headers['Content-Type'], 'application/rss'
+  end
 end
